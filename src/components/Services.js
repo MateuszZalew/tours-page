da2a58f5520fb78f277655648cc1bd054348e7c4
zalesiu <zalewmat31@gmail.com>
2023-08-24 15:48:54 +0200

    format content in Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,49 +1,25 @@
 import Title from "./Title";
+import { servicesData } from "../data";
 
 const Services = () => {
+  const services = servicesData.map((service) => {
+    const { id, title, icon, text } = service;
+    return (
+      <article key={id} className="service">
+        <span className="service-icon">
+          <i className={icon}></i>
+        </span>
+        <div className="service-info">
+          <h4 className="service-title">{title}</h4>
+          <p className="service-text">{text}</p>
+        </div>
+      </article>
+    );
+  });
   return (
     <section className="section services" id="services">
       <Title title="our" subtitle="services" />
-      <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-wallet fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-tree fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-socks fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-      </div>
+      <div className="section-center services-center">{services}</div>
     </section>
   );
 };
