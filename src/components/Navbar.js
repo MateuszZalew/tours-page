45e4a956af888f155757d689024211afd70e18d6
zalesiu <zalewmat31@gmail.com>
2023-08-24 15:20:55 +0200

    add data.js, update links in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,42 @@
+import logo from "../images/logo.svg";
+import { pageLinks, socials } from "../data";
+
 const Navbar = () => {
+  const navLinks = pageLinks.map((page) => (
+    <li key={page.id}>
+      <a href={page.href} className="nav-link">
+        {page.name}
+      </a>
+    </li>
+  ));
+
+  const socialMediaLinks = socials.map((social) => (
+    <li key={social.id}>
+      <a
+        href={social.href}
+        target="_blank"
+        className="nav-icon"
+        rel="noreferrer"
+      >
+        <i className={`fab fa-${social.name}`}></i>
+      </a>
+    </li>
+  ));
+
   return (
-    <nav class="navbar">
-      <div class="nav-center">
-        <div class="nav-header">
-          <img src="./images/logo.svg" class="nav-logo" alt="backroads" />
-          <button type="button" class="nav-toggle" id="nav-toggle">
-            <i class="fas fa-bars"></i>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
           </button>
         </div>
-        <ul class="nav-links" id="nav-links">
-          <li>
-            <a href="#home" class="nav-link">
-              {" "}
-              home{" "}
-            </a>
-          </li>
-
-          <li>
-            <a href="#about" class="nav-link">
-              {" "}
-              about{" "}
-            </a>
-          </li>
-
-          <li>
-            <a href="#services" class="nav-link">
-              {" "}
-              services{" "}
-            </a>
-          </li>
-
-          <li>
-            <a href="#tours" class="nav-link">
-              {" "}
-              tours
-            </a>
-          </li>
+        <ul className="nav-links" id="nav-links">
+          {navLinks}
         </ul>
 
-        <ul class="nav-icons">
-          <li>
-            <a href="https://www.twitter.com" target="_blank" class="nav-icon">
-              <i class="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" class="nav-icon">
-              <i class="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" class="nav-icon">
-              <i class="fab fa-squarespace"></i>
-            </a>
-          </li>
-        </ul>
+        <ul className="nav-icons">{socialMediaLinks}</ul>
       </div>
     </nav>
   );
---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const pageLinks = [
+  {
+    id: 1,
+    name: "home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "about",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "services",
+    href: "#services",
+  },
+  {
+    id: 4,
+    name: "tours",
+    href: "#tours",
+  },
+];
+
+export const socials = [
+  {
+    id: 1,
+    name: "twitter",
+    href: "https://www.twitter.com",
+  },
+  {
+    id: 2,
+    name: "facebook",
+    href: "https://www.facebook.com",
+  },
+  {
+    id: 3,
+    name: "squarespace",
+    href: "https://www.squarespace.com",
+  },
+];
