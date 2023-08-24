f750136e8cec75a4c3d718c13dd4dc58cb1720d7
zalesiu <zalewmat31@gmail.com>
2023-08-25 01:40:39 +0200

    refactor footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,60 +1,35 @@
+import { pageLinks, socials } from "../data";
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  const navLinks = pageLinks.map((page) => (
+    <li key={page.id}>
+      <a href={page.href} className="footer-link">
+        {page.name}
+      </a>
+    </li>
+  ));
+
+  const socialMediaLinks = socials.map((social) => (
+    <li key={social.id}>
+      <a
+        href={social.href}
+        target="_blank"
+        className="nav-icon"
+        rel="noreferrer"
+      >
+        <i className={`fab fa-${social.name}`}></i>
+      </a>
+    </li>
+  ));
   return (
     <footer className="section footer">
-      <ul className="footer-links">
-        <li>
-          <a href="#home" className="footer-link">
-            home
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="footer-link">
-            about
-          </a>
-        </li>
-        <li>
-          <a href="#services" className="footer-link">
-            services
-          </a>
-        </li>
-        <li>
-          <a href="#featured" className="footer-link">
-            featured
-          </a>
-        </li>
-      </ul>
-      <ul className="footer-icons">
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-          >
-            <i className="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-          >
-            <i className="fab fa-squarespace"></i>
-          </a>
-        </li>
-      </ul>
+      <ul className="footer-links">{navLinks}</ul>
+      <ul className="footer-icons">{socialMediaLinks}</ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{currentYear}</span> all rights reserved
       </p>
     </footer>
   );
